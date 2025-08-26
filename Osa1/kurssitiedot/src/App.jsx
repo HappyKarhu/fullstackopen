d48d0b16b15b8ebfee0aa2ac7d6fde4738251d9d
HappyKarhu <mojca.jan@gmail.com>
2025-08-26 09:11:03 +0300

    1.2: kurssitiedot, step2

--- Osa1/kurssitiedot/src/App.jsx
+++ Osa1/kurssitiedot/src/App.jsx
@@ -2,18 +2,16 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
+const Part = (props) => {
+  return <p>{props.name} {props.exercises}</p>
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
@@ -33,15 +31,16 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  
   return (
     <div>
       <Header course={course} />
       <Content
-        part1={part1} exercises1={exercises1}
+      part1={part1} exercises1={exercises1}
         part2={part2} exercises2={exercises2}
         part3={part3} exercises3={exercises3}
       />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
