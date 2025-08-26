ccbacc9626e02db1598421a4d3d0ef5727b1197a
HappyKarhu <mojca.jan@gmail.com>
2025-08-26 12:20:32 +0300

    1.7: unicafe step2

--- Osa1/unicafe/src/App.jsx
+++ Osa1/unicafe/src/App.jsx
@@ -6,11 +6,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+// palaute
   const handleFeedback = (type) => {
     if (type === 'good') setGood(good + 1)
     else if (type === 'neutral') setNeutral(neutral + 1)
     else if (type === 'bad') setBad(bad + 1)
   }
+
+  
+  const yhteenlasketunMaara = good + neutral + bad
+  // keskiarvo - neutraali on 0
+  const keskiarvo = yhteenlasketunMaara === 0 ? 0 : (good - bad) / yhteenlasketunMaara
+  const prosenttiaPosiitivisaaPalautteista = yhteenlasketunMaara === 0 ? 0 : (good / yhteenlasketunMaara) * 100
 
   return (
     <div>
@@ -19,11 +26,15 @@ const App = () => {
       <button onClick={() => handleFeedback('neutral')}>neutral</button>
       <button onClick={() => handleFeedback('bad')}>bad</button>
     
-
       <h1>statistics of feedbacks</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+
+      <p>all {yhteenlasketunMaara}</p>
+      <p>average {keskiarvo}</p>
+      <p>positive {prosenttiaPosiitivisaaPalautteista}</p>
+
     </div>
   )
 }
