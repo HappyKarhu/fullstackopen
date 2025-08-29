ce9f8e1f4453f4ef750876128d78f8e89fd88cae
HappyKarhu <mojca.jan@gmail.com>
2025-08-29 13:15:13 +0300

    2.1: kurssitiedot step6

---
+++ Osa2/kurssitiedot/src/content.jsx
@@ -0,0 +1,11 @@
+import Part from './part'
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+export default Content
