32d37f34530ab99307af3e8d2a5209a2e446f576
HappyKarhu <mojca.jan@gmail.com>
2025-09-03 09:41:24 +0300

    2.9*: puhelinluettelo step4-filter

--- Osa2/puhelinluettelo/src/App.jsx
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' },
-    { name: 'Grace Hopper', number: '+31-231-12314' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [newName, setNewName] = useState('')  //nameinput
+  const [newNumber, setNewNumber] = useState('') //number input
+  const [filter, setFilter] = useState('')   //filter input
 
   const handleNameInput = (event) => {
     setNewName(event.target.value)
@@ -14,12 +17,14 @@ const App = () => {
   const handleNumberInput = (event) => { //updates number and up-names
     setNewNumber(event.target.value)
   }
+  const handleFilterInput = (event) => {
+    setFilter(event.target.value)
+  }
 
   const addContact = (event) => {
     event.preventDefault()       // prevent the default action of submitting HTML forms!-page refresh
     
     const newNameLowerCase = newName.toLowerCase() //converts into lowercase, ignores letter's size
-
 
     const ifDuplicate = persons.some(person => person.name.toLowerCase() === newNameLowerCase)
 
@@ -37,9 +42,13 @@ const App = () => {
     setNewNumber('')
   }
 
+  const filterShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={filter} onChange={handleFilterInput} /></div>
+      <h2>Add a new person</h2>
       <form onSubmit={addContact}>
         <div>
           name: <input value={newName} onChange={handleNameInput} />
@@ -50,13 +59,12 @@ const App = () => {
 
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
+        {filterShow.map((person) => (
         <div key={person.name}>{person.name} {person.number}</div>
         ))}
       </div>
     </div>
   )
-
 }
 
 export default App
