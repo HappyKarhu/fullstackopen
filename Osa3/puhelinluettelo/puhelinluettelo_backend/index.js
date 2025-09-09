74d0ad962631212cf4ad5b540cef8d9b15c555b3
HappyKarhu <mojca.jan@gmail.com>
2025-09-09 13:05:40 +0300

    3.7: puhelinluettelon backend step7-MOrgan

--- Osa3/puhelinluettelo/puhelinluettelo_backend/index.js
+++ Osa3/puhelinluettelo/puhelinluettelo_backend/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny')) //predefined format string-it logs the method, URL, status, response time and content length
 
 let persons = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
