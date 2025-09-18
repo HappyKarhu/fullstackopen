fad0aa2312d083f91c4998677f39c83e229903d6
HappyKarhu <mojca.jan@gmail.com>
2025-09-18 19:28:11 +0300

    .5*: apufunktioita ja yksikkötestejä, step3

--- Osa4/blogilista/tests/favoriteBlog.test.js
+++ Osa4/blogilista/tests/favoriteBlog.test.js
@@ -2,28 +2,6 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
-  assert.strictEqual(listHelper.dummy(blogs), 1)
-})
-
-describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  test('when list has only one blog, equals the likes of that', () => {
-    assert.strictEqual(listHelper.totalLikes(listWithOneBlog), 5)
-  })
-})
-
 describe('favorite blog', () => {
   const blogs = [
     {
--- Osa4/blogilista/utils/list_helper.js
+++ Osa4/blogilista/utils/list_helper.js
@@ -3,7 +3,13 @@ const dummy = (blogs) => 1
 const totalLikes = (blogs) =>
   blogs.reduce((sum, blog) => sum + (blog.likes || 0), 0)
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  return blogs.reduce((favorite, blog) => (blog.likes > (favorite.likes || 0)) ? blog : favorite, {})
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
