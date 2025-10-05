882a2aea12bc3d5de466433baef9d741b9960399
HappyKarhu <mojca.jan@gmail.com>
2025-10-05 16:12:37 +0300

    5.14: blogilistan testit, step2

--- Osa5/blogilista/frontend/src/components/Blog.test.jsx
+++ Osa5/blogilista/frontend/src/components/Blog.test.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { test, expect } from 'vitest'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 test('renders title and author', () => {
   const blog = {
@@ -21,4 +22,21 @@ const titleAuthorDiv = screen.getByText(
   // Check that url and likes are not rendered
   expect(screen.queryByText(blog.url)).toBeNull()
   expect(screen.queryByText(`likes ${blog.likes}`)).toBeNull()
+})
+
+test('shows url and likes after clicking the view button', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Blogger1',
+    url: 'https://example.com',
+    likes: 5
+  }
+
+  render(<Blog blog={blog} updateBlog={() => {}} deleteBlog={() => {}} />)
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(screen.getByText(blog.url)).toBeDefined()
+  expect(screen.getByText(`likes ${blog.likes}`)).toBeDefined()
 })
