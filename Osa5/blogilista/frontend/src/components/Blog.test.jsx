8d41bf9f84c82e72b706c5838e5ab3cc9335f37d
HappyKarhu <mojca.jan@gmail.com>
2025-10-06 08:42:23 +0300

    5.15: blogilistan testit, step3

--- Osa5/blogilista/frontend/src/components/Blog.test.jsx
+++ Osa5/blogilista/frontend/src/components/Blog.test.jsx
@@ -4,7 +4,13 @@ import { test, expect } from 'vitest'
 import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
 
-test('renders title and author', () => {
+vi.mock('../services/blogs', () => ({
+  default: {
+    updateBlog: vi.fn().mockResolvedValue({}),
+  },
+}))
+//test 5.13
+test('test forrenders title and author', () => {
   const blog = {
     title: 'Component testing is done with react-testing-library',
     author: 'Blogger1',
@@ -24,7 +30,8 @@ const titleAuthorDiv = screen.getByText(
   expect(screen.queryByText(`likes ${blog.likes}`)).toBeNull()
 })
 
-test('shows url and likes after clicking the view button', async () => {
+//test 5.14
+test('test forshows url and likes after clicking the view button', async () => {
   const blog = {
     title: 'Component testing is done with react-testing-library',
     author: 'Blogger1',
@@ -39,4 +46,25 @@ test('shows url and likes after clicking the view button', async () => {
 
   expect(screen.getByText(blog.url)).toBeDefined()
   expect(screen.getByText(`likes ${blog.likes}`)).toBeDefined()
+})
+
+//test 5.15
+test('calls updateBlog handler twice when like button is clicked twice', async () => {
+  const blog = {
+    id: '12345',
+    title: 'Component testing is done with react-testing-library',
+    author: 'Blogger1',
+    url: 'https://example.com',
+    likes: 5
+  }
+  const updateBlog = vi.fn()
+
+  render(<Blog blog={blog} updateBlog={updateBlog} deleteBlog={() => {}} />)
+  const user = userEvent.setup()
+  await user.click(screen.getByText('view'))
+  const likeButton = screen.getByText('like')
+  //2times, to click like button twice
+  await user.click(likeButton)
+  await user.click(likeButton)
+  expect(updateBlog).toHaveBeenCalledTimes(2)
 })
