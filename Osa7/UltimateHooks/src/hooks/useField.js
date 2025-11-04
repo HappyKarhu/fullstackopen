a5dcc9c71814b54cd65a792d441428c6c1f6773b
HappyKarhu <mojca.jan@gmail.com>
2025-11-04 09:58:39 +0200

    7.8: ultimate hooks

---
+++ Osa7/UltimateHooks/src/hooks/useField.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export default useField
