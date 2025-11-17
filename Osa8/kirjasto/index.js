9f8d6484937ed406cb3aff5c35da37183f18bf6c
HappyKarhu <mojca.jan@gmail.com>
2025-11-17 10:01:16 +0200

    8.3: kaikki kirjailijat

--- Osa8/kirjasto/index.js
+++ Osa8/kirjasto/index.js
@@ -97,10 +97,16 @@ type Book {
     genres: [String!]!
 }
 
+type Author {
+    name: String!
+    bookCount: Int!
+}
+
 type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -108,7 +114,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors.map(author => ({
+      name: author.name,
+      bookCount: books.filter(book => book.author === author.name).length
+    }))
   }
 }
 
