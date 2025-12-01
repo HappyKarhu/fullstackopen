0d551daa186ff9657c4cad1df4213b9225771c58
HappyKarhu <mojca.jan@gmail.com>
2025-12-01 14:56:28 +0200

    9.7 WebExercises &tested with Postman

--- Osa9/index.ts
+++ Osa9/index.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
+//hello endpoint
 app.get('/hello', (_req: Request, res: Response) => {
   res.send('Hello Full Stack!');
 });
 
+
+//bmi calvculator endpoint
 app.get('/bmi', (req: Request, res: Response) => {
   const height = req.query.height;
   const weight = req.query.weight;
@@ -23,6 +28,23 @@ app.get('/bmi', (req: Request, res: Response) => {
     bmi
   });
 });
+
+//excercise calculator endpoint
+app.post('/exercises', (req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { daily_exercises, target } =req.body;
+    if (!daily_exercises || target === undefined) {
+      return res.status(400).json({ error: 'parameters missing' });
+    }
+
+    //valideta numeric array and target
+    if (!Array.isArray(daily_exercises) || daily_exercises.some((n: any) => isNaN(Number(n))) || isNaN(Number(target))) {
+        return res.status(400).json({ error: 'malformatted parameters' });
+    }
+    const result = calculateExercises(daily_exercises.map((n: any) => Number(n)), Number(target));
+    return res.json(result);
+});
+
 
 const PORT = 3003;
 app.listen(PORT, () => {
